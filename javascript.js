9b0397c3173fe37d6ad0ec987fee0aacc65117dd
RodrigoNogueira2020 <rodrigo.a.nogueira@gmail.com>
2021-05-25 07:59:41 +0100

    atm

--- javascript.js
+++ javascript.js
@@ -116,3 +116,173 @@ class Revisao{
         }
     }
 }
+
+/*var selectedRow = null;
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["dataDeNascimento"] = document.getElementById("dataDeNascimento").value;
+    formData["Genero"] = document.getElementById("Genero").value;
+    formData["Email"] = document.getElementById("Email").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.dataDeNascimento;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.Genero;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.Email;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Editar</a>
+                       <a onClick="onDelete(this)">Apagar</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("dataDeNascimento").value = "";
+    document.getElementById("Genero").value = "";
+    document.getElementById("Email").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("dataDeNascimento").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Genero").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Email").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.dataDeNascimento;
+    selectedRow.cells[2].innerHTML = formData.Genero;
+    selectedRow.cells[3].innerHTML = formData.Email;
+}
+
+function onDelete(td) {
+    if (confirm('Deseja apagar este registo?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("tabela").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+*/
+
+// CRUD VIDEO
+
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+        }
+        else{
+            updateRecord(formData);
+        }
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["dataDeNascimento"] = document.getElementById("dataDeNascimento").value;
+    formData["Genero"] = document.getElementById("Genero").value;
+    formData["Email"] = document.getElementById("Email").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.dataDeNascimento;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.Genero;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.Email;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("dataDeNascimento").value = "";
+    document.getElementById("Genero").value = "";
+    document.getElementById("Email").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("dataDeNascimento").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Genero").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Email").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.dataDeNascimento;
+    selectedRow.cells[2].innerHTML = formData.Genero;
+    selectedRow.cells[3].innerHTML = formData.Email;
+}
+
+function onDelete(td) {
+    if (confirm('Deseja eliminar este registo?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide")){
+            document.getElementById("fullNameValidationError").classList.add("hide");
+        }
+    }
+    return isValid;
+}
